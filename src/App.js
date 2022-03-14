9fc4854e1a6a0dbd70c5d44b93eea516ef6058d1
provideforme <evan.a.bernard@gmail.com>
2022-03-14 15:10:42 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const floorplan = [
+    {
+      room: "Kitchen",
+      appliances: ['Oven', 'Sink']
+    },
+    {
+      room: "Bedroom"
+    },
+    {
+      room: "Bathroom"
+    }
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>sup</h1>
     </div>
   );
 }
